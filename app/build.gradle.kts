b77ebfde9623b185171fd8621100ff3501781aa5
ean3ena <ean3ena@yandex.ru>
2024-10-07 11:36:18 +0300

    init commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("application")
+    id("checkstyle")
+    id("jacoco")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+}
